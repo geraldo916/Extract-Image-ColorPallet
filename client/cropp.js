1cd99e3091e2097ca0a3364b72c544bfce77800a
Geraldo Munhika <geraldosamuel916@gmail.com>
2023-08-25 06:30:48 +0100

    add bidirecional crop movement

--- client/cropp.js
+++ client/cropp.js
@@ -1,5 +1,5 @@
 const cropBox = document.getElementById('crop-box');
-const boxContainer = document.getElementById('canvas-container');
+const boxContainer = document.getElementById('crop-limitator');
 
 class Cropper{
     mouseState = null;
@@ -11,66 +11,84 @@ class Cropper{
     height;
     cropBoxLeftBounding;
     cropBoxTopBounding;
+    parentLeft;
+    parentTop;
     constructor(parentLeft, parentTop){
+        this.parentLeft = parentLeft;
+        this.parentTop = parentTop;
+        this.mouseState = null;
         this.cropBoxLeftBounding = Math.round(cropBox.getBoundingClientRect().left);
         this.cropBoxTopBounding = Math.round(cropBox.getBoundingClientRect().top);
-        this.mouseState = null;
-        this.startX = this.cropBoxLeftBounding - parentLeft;
-        this.startY = this.cropBoxTopBounding - parentTop;
+        this.startX = this.cropBoxLeftBounding - this.parentLeft;
+        this.startY = this.cropBoxTopBounding - this.parentTop;
         this.endX = cropBox.offsetWidth + this.startX;
         this.endY = cropBox.offsetHeight + this.startY;
-        this.width = cropBox.clientWidth;
-        this.height = cropBox.clientHeight;
-        console.log("Crop Left - Top:",this.startX,this.startY);
     }
 
     /**
      * @returns 
      */
     getCoordenates(){
+        let inicialcropBoxLeftBounding = Math.round(cropBox.getBoundingClientRect().left);
+        let inicialcropBoxTopBounding = Math.round(cropBox.getBoundingClientRect().top);
+        let inicialstartX = inicialcropBoxLeftBounding - this.parentLeft;
+        let inicialstartY = inicialcropBoxTopBounding - this.parentTop;
+        let inicialendX = cropBox.offsetWidth + inicialstartX;
+        let inicialendY = cropBox.offsetHeight + inicialstartY;
+
         return {
-            startX:this.startX,
-            startY:this.startY,
-            endX: this.endX,
-            endY: this.endY
+            startX: inicialstartX,
+            startY: inicialstartY,
+            endX:  inicialendX,
+            endY:  inicialendY
+        }
+    }
+
+    /**
+     * @returns 
+     */
+    getInicialCoords(){
+        return {
+            startX: this.startX,
+            startY: this.startY,
+            endX:  this.endX,
+            endY:  this.endY
         }
     }
 
     moveCropBox(){
         let startX = 0;
         let startY = 0;
-        let movedPixelsX = 0;
-        let movedPixelsY = 0;
         let pixelToMoveX = 0;
         let pixelToMoveY = 0;
 
         cropBox.addEventListener('mousedown',(e)=>{
-            this.mouseState = 'move';
+
             startX = e.clientX;
             startY = e.clientY;
+            this.mouseState = 'move';
         })
         
         boxContainer.addEventListener('mousemove',(e)=>{
             if(this.mouseState === 'move'){
-                movedPixelsX = e.clientX;
-                movedPixelsY = e.clientY;
-                pixelToMoveX = movedPixelsX - startX;
-                pixelToMoveY = movedPixelsY - startY;
+                let coords = this.getInicialCoords();
+                pixelToMoveX = e.clientX - startX;
+                pixelToMoveY = e.clientY - startY;
 
                 if(pixelToMoveX != 0 || pixelToMoveY != 0){
-                    cropBox.style.transform = `translateX(${pixelToMoveX}px) translateY(${pixelToMoveY}px)`
+                    cropBox.style.transform = `translateX(${pixelToMoveX+coords.startX}px) translateY(${pixelToMoveY+coords.startY}px)`
                 }
             }
         })
 
         boxContainer.addEventListener('mouseup',(e)=>{
             this.mouseState = 'not drawing';
-            this.startX = pixelToMoveX < 0 ? 0 : pixelToMoveX;
-            this.startY = pixelToMoveY < 0 ? 0 : pixelToMoveY;
-
-            this.endX = this.startX + this.width;
-            this.endY = this.startY + this.height;
             let coords = this.getCoordenates();
+            this.startX = coords.startX;
+            this.startY = coords.startY;
+            this.endX = coords.endX;
+            this.endY = coords.endY;
+            
         })
 
     }
